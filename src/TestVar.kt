ab56c612c484b0ba8427e466ecfaeba3fd43dfb8
Sreythou Khean <kheansreythou19@gmail.com>
2018-08-02 09:57:45 +0700

    Introduction of Kotlin Code.

---
+++ src/TestVar.kt
@@ -0,0 +1,14 @@
+fun main(args:Array<String>){
+	var numVar:Int=10
+	var floatVar:Float=3.14f
+	var charVar:Char='A'
+	var strVar:String="Sreythou"
+	var booleanVar=true
+	println(numVar)
+	println(floatVar)
+	println(charVar)
+	println(strVar)
+	println(booleanVar)
+	println(""+numVar)
+	println(floatVar.toString())
+}
